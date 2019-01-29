41247a28326fb6e8beadbffb2e56d163e43548be
David Wursteisen <david.wursteisen@margo.com>
2019-01-29 11:16:42 +0100

    intellij gradle composite build

---
+++ clone.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/dwursteisen/beat-the-high-score
+git clone https://github.com/dwursteisen/libgdx-addons
+
+cp settings.gradle.template beat-the-high-score/settings.gradle
